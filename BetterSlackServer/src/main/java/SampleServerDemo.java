40552ab4186e87a382674d6d16866253431be932
Zuravvski <m.zurawski1994@gmail.com>
2019-03-03 04:55:30 +0100

    Asynchronous accepting added

--- BetterSlackServer/src/main/java/SampleServerDemo.java
+++ BetterSlackServer/src/main/java/SampleServerDemo.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 
 public class SampleServerDemo {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SampleServer sampleServer = new SampleServer();
         sampleServer.startServer(55555);
+        Thread.sleep(5000);
+        sampleServer.shutdown();
     }
 }
